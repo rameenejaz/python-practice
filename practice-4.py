ae4c1828a068aed379e4ffd6f7c022a3f1a96e76
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-22 01:19:08 +0500

    learning about arrays in python

---
+++ practice-4.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    return a+b
+x=2
+y=3
+print("the sum of x and y is:",add(x,y))
---
+++ practice-5.py
@@ -0,0 +1,4 @@
+arr=[1,2,3,4,5]
+print(f"the value at 0 index is", arr[0])
+for i in range (arr[4]):
+    print(arr[i])
