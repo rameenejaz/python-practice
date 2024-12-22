d7c23c62da2e351cdc312854e5254f8ff2be0cf1
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-22 15:49:25 +0500

    greeting using functions

---
+++ q1a.py
@@ -0,0 +1,4 @@
+def greet(name):
+    print(f"Hello {name}!")
+name=input("Enter your name:")
+greet(name)
--- vowelsStrings.py
+++ vowelsStrings.py
@@ -1,7 +1,15 @@
 sentence=str(input("Enter a sentence to find the vowels in it: "))
 vowels="aeiou"
 countVowel=0
-for char in sentence.lower(): #for case sensitive
+vowel_list=[]
+character_list=[]
+characterCount=0
+for char in sentence.lower():
+    character_list.append(char)
+    characterCount+=1
     if char in vowels:
         countVowel+=1
-print(f"The sentence contains {countVowel} vowels")
+        vowel_list.append(char)
+print(f"The sentence contains {countVowel} vowels, the vowels are: {vowel_list}")
+print(f"The characters are: {character_list}")
+print(f"There are {characterCount-1} characters")
