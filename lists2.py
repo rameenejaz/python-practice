0d7af92ceb2e5bb4b504520f68ec848bab4933f3
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-28 23:16:52 +0500

    working with tuples

---
+++ lists2.py
@@ -0,0 +1,8 @@
+#using lists as stacks
+a=["a","b","c","d"]
+print(a)
+a.append("e")
+print(a)
+a.pop()
+print(a)
+a.pop()
---
+++ tuples-1.py
@@ -0,0 +1,6 @@
+empty_tuple=()
+single_element_tuple=(5,)
+mixed_tuple=(1,"Hello",3.14)
+print(mixed_tuple[1])
+print(f"The number of times 3.14 occured: {mixed_tuple.count(3.14)}")
+
